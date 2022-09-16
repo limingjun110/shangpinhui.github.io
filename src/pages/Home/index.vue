7897ef78be593d61f051d711c60c94b303cb7980
limingjun110 <110209752+limingjun110@users.noreply.github.com>
2022-09-16 10:46:15 +0800

    Create index.vue

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor v-for="floor in floorList" :key="floor.id" :list="floor"></Floor>
+    <Brand></Brand>
+  </div>
+</template>
+
+<script>
+import ListContainer from '@/pages/Home/ListContainer'
+import Recommend from './Recommend/index.vue'
+import Rank from './Rank/index.vue'
+import Like from './Like/index.vue'
+import Floor from './Floor/index.vue'
+import Brand from './Brand/index.vue'
+import { mapState } from 'vuex'
+export default {
+  name: 'MyHome',
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  },
+  mounted() {
+    this.$store.dispatch('getFloorList')
+    this.$store.dispatch('getUserInfo')
+  },
+  computed: {
+    ...mapState({
+      floorList: state => state.home.floorList
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
