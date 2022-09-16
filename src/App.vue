60b3ee4067e78b15eb8aea55d7030c766f8c6dba
limingjun110 <110209752+limingjun110@users.noreply.github.com>
2022-09-16 10:44:10 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header></Header>
+    <router-view></router-view>
+    <Footer v-show="$route.meta.show"></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header/index.vue'
+import Footer from './components/Footer/index.vue'
+export default {
+  name: 'MyApp',
+  components: {
+    Header,
+    Footer
+  },
+  mounted() {
+    this.$store.dispatch('categoryList')
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
