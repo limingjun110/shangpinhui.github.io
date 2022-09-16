6a953b216f06f430865513fb607d66bdea9e69e8
limingjun110 <110209752+limingjun110@users.noreply.github.com>
2022-09-16 10:48:11 +0800

    Create home.js

---
+++ src/store/home.js
@@ -0,0 +1,44 @@
+import { reqCategoryList, reqGetBannerList, reqGetFloorList } from '@/api'
+const state = {
+  categoryList: [],
+  bannerList: [],
+  floorList: []
+}
+const mutations = {
+  CATEGORYLIST(state, categoryList) {
+    state.categoryList = categoryList
+  },
+  GETBANNERLIST(state, bannerList) {
+    state.bannerList = bannerList
+  },
+  GETFLOORLIST(state, floorList) {
+    state.floorList = floorList
+  }
+}
+const actions = {
+  async categoryList({ commit }) {
+    const result = await reqCategoryList()
+    if (result.code === 200) {
+      commit('CATEGORYLIST', result.data)
+    }
+  },
+  async getBannerList({ commit }) {
+    const result = await reqGetBannerList()
+    if (result.code === 200) {
+      commit('GETBANNERLIST', result.data)
+    }
+  },
+  async getFloorList({ commit }) {
+    const result = await reqGetFloorList()
+    if (result.code === 200) {
+      commit('GETFLOORLIST', result.data)
+    }
+  }
+}
+const getters = {}
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
